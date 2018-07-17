5f9dd5172d85d8c01f49dfa3e4d91ff3a4045e6c
Roberto Ribeiro <robertov82008@gmail.com>
2018-07-17 16:12:47 -0300

    Componente Rodapé

--- frontend/src/components/template/Footer/Footer.js
+++ frontend/src/components/template/Footer/Footer.js
@@ -3,5 +3,7 @@ import './Footer.css';
 
 export default () =>
     <footer className='footer'>
-        Rodapé
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger' /> por <strong>Cod<span className='text-danger'>3</span>r</strong>
+        </span>
     </footer>;
