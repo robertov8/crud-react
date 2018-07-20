43212d3d054e15e42b25e504b1635b2feeff389a
Roberto Ribeiro <robertov82008@gmail.com>
2018-07-20 18:08:04 -0300

    Componente Cadastro Usuário #03

--- frontend/src/components/user/UserCrud.js
+++ frontend/src/components/user/UserCrud.js
@@ -18,6 +18,10 @@ export default class UserCrud extends Component {
     constructor(props) {
         super(props);
         this.state = { ...initialState };
+
+        this.updateField = this.updateField.bind(this);
+        this.save = this.save.bind(this);
+        this.clear = this.clear.bind(this);
     }
 
     clear() {
@@ -41,10 +45,62 @@ export default class UserCrud extends Component {
         return list;
     }
 
+    updateField(event) {
+        const user = { ...this.state.user };
+        user[event.target.name] = event.target.value;
+        this.setState({ user });
+    }
+
+    renderForm() {
+        return (
+            <div className='form'>
+                <div className='row'>
+                    <div className='col-12 col-md-6'>
+                        <div className='form-group'>
+                            <label>Nome</label>
+                            <input
+                                type='text'
+                                className='form-control'
+                                name='name'
+                                value={this.state.user.name}
+                                placeholder='Digite um nome'
+                                onChange={this.updateField}/>
+                        </div>
+                    </div>
+
+                    <div className='col-12 col-md-6'>
+                        <div className='form-group'>
+                            <label>E-mail</label>
+                            <input
+                                type='text'
+                                className='form-control'
+                                name='email'
+                                value={this.state.user.email}
+                                placeholder='Digite um e-mail'
+                                onChange={this.updateField} />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className='row'>
+                    <div className='col-12 d-flex justify-content-end'>
+                        <button className='btn btn-primary' onClick={this.save}>Salvar</button>
+                        <button className='btn btn-secondary ml-2' onClick={this.clear}>Cancelar</button>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+
     render() {
         return (
             <Main {...headerProps} >
                 Cadastro de Usuário
+
+                {this.renderForm()}
             </Main>
         );
     }
